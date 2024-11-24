62398d52a9e141ccbc0e396ef020df4fc1c5cf02
Anil Patel <anilkupatel001@gmail.com>
2024-11-24 23:16:54 +0000

    updating role and removing deprecated part

--- role.tf
+++ role.tf
@@ -1,9 +1,10 @@
 locals {
-  role_name = "${var.cluster_name}-role"
+  cluster_role_name = "${var.cluster_name}-cluster-role"
+  node_role_name = "${var.cluster_name}-node-role"
 }
 
 resource "aws_iam_role" "main" {
-  name               = local.role_name
+  name               = local.cluster_role_name
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
@@ -20,25 +21,25 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "main-AmazonEKSClusterPolicy" {
+resource "aws_iam_role_policy_attachment" "cluster-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.main.name
 }
 
-resource "aws_iam_role_policy_attachment" "main-AmazonEKSServicePolicy" {
+resource "aws_iam_role_policy_attachment" "cluster-AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
   role       = aws_iam_role.main.name
 }
 
 # Optionally, enable Security Groups for Pods
 # Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
-resource "aws_iam_role_policy_attachment" "main-AmazonEKSVPCResourceController" {
+resource "aws_iam_role_policy_attachment" "cluster-AmazonEKSVPCResourceController" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.main.name
 }
 
 resource "aws_iam_role" "node_role" {
-  name = "eks-node-role"
+  name = local.node_role_name
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -53,9 +54,19 @@ resource "aws_iam_role" "node_role" {
     ]
   })
 
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
-  ]
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node_role.name
 }
