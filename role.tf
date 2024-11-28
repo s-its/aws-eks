e8bd233647e1d7960148357a3c21b8841a2580c4
Anil Patel <anilkupatel001@gmail.com>
2024-11-28 20:12:47 +0000

    adding oidc

--- role.tf
+++ role.tf
@@ -1,6 +1,7 @@
 locals {
   cluster_role_name = "${var.cluster_name}-cluster-role"
   node_role_name    = "${var.cluster_name}-node-role"
+  oidc_role_name = "${var.cluster_name}-oidc-role"
 }
 
 resource "aws_iam_role" "main" {
@@ -84,4 +85,37 @@ resource "aws_iam_role_policy_attachment" "node-AmazonEC2ContainerRegistryReadOn
 resource "aws_iam_role_policy_attachment" "node-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.node_role.name
+}
+
+resource "aws_iam_openid_connect_provider" "oidc" {
+  url            = aws_eks_cluster.cluster.identity[0].oidc.issuer
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = ["123DBB2CFA54152664053A53D7E34C1A"] # Default AWS OIDC thumbprint
+}
+
+resource "aws_iam_role" "ebs_csi_driver_role" {
+  name = local.oidc_role_name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.oidc.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${replace(aws_eks_cluster.cluster.identity[0].oidc.issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_driver_policy" {
+  role       = aws_iam_role.ebs_csi_driver_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
