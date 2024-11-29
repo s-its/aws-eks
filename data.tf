7d8556c1bfca1d5c7631d5d6f0dbd3c5d4128e3d
Anil Patel <anilkupatel001@gmail.com>
2024-11-29 00:01:07 +0000

    adding pod identification role to ebs addon

--- data.tf
+++ data.tf
@@ -24,3 +24,13 @@ data "aws_iam_policy_document" "ebs_assume_role_policy" {
     ]
   }
 }
+
+data "tls_certificate" "example" {
+  url = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "example" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.example.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+}
