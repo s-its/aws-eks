1ea45e59e350a1716f46263dc6d49c320e210d37
Anil Patel <anilkupatel001@gmail.com>
2024-11-30 18:16:28 +0000

    updating calico cr config

--- versions.tf
+++ versions.tf
@@ -16,3 +16,18 @@ terraform {
     }
   }
 }
+
+provider "helm" {
+  kubernetes{
+    host                   = aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.main.token
+  }
+
+}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.main.token
+}
