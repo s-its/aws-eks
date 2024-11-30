9849dc1f93dd1e7b5e608516ec54e12886026647
Anil Patel <anilkupatel001@gmail.com>
2024-11-30 18:33:14 +0000

    formatting

--- versions.tf
+++ versions.tf
@@ -6,13 +6,5 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.46"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "2.16.1"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.34.0"
-    }
   }
 }
