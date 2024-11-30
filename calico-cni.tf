d0c843c90ccdaf1c9f7d7d4ebe7e201838038263
Anil Patel <anilkupatel001@gmail.com>
2024-11-30 18:04:46 +0000

    updating manifest file

--- calico-cni.tf
+++ calico-cni.tf
@@ -10,20 +10,27 @@ resource "helm_release" "calico" {
 
 resource "kubernetes_manifest" "calico_cr" {
   depends_on = [aws_eks_cluster.cluster]
-  manifest = <<EOF
-apiVersion: operator.tigera.io/v1
-kind: Installation
-metadata:
-  name: default
-spec:
-  cni:
-    type: Calico
-  calicoNetwork:
-    ipPools:
-    - blockSize: 26
-      cidr: 192.168.0.0/16
-      encapsulation: VXLAN
-      natOutgoing: Enabled
-      nodeSelector: all()
-EOF
+  manifest = jsonencode({
+    apiVersion = "operator.tigera.io/v1"
+    kind       = "Installation"
+    metadata = {
+      name = "default"
+    }
+    spec = {
+      cni = {
+        type = "Calico"
+      }
+      calicoNetwork = {
+        ipPools = [
+          {
+            blockSize    = 26
+            cidr         = "192.168.0.0/16"
+            encapsulation = "VXLAN"
+            natOutgoing  = "Enabled"
+            nodeSelector = "all()"
+          }
+        ]
+      }
+    }
+  })
 }
